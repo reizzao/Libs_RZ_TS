fb886f5c6fae2d19163b8cf171910ba09ad1a66b
reizzao <reizao1977@gmail.com>
2025-05-16 15:46:59 -0300

    todo: criando libTS

--- src/Domain/Repositories/ILibsTSRepository.ts
+++ src/Domain/Repositories/ILibsTSRepository.ts
@@ -2,8 +2,7 @@ import { LibsTSModel } from "../Model/LibsTS.ts";
 import { ILibsTSDTO } from "../UseCases/CreateLibsTS/dto.ts";
 
 interface ILibsTSRepository {
-  // Console(...args: any[]): void
-  create (lib: ILibsTSDTO): Promise<LibsTSModel>
+  create(lib: ILibsTSDTO): Promise<LibsTSModel>
 }
 
 export type { ILibsTSRepository }
